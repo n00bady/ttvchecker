8ae5ad5c0fa49a6778b9a1fca5cfa34521c547b2
Jon Doe <2362996-n00bady@users.noreply.gitlab.com>
2023-03-05 16:41:18 +0200

    Made parse() to return errors.

--- parse.go
+++ parse.go
@@ -3,30 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func parse(response *http.Response) bool {
+func parse(response *http.Response) (bool, error) {
   islive := false
   
   doc, err := goquery.NewDocumentFromReader(response.Body)
   if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
+    return false, err
   }
   doc.Find("script").Each(func(i int, s *goquery.Selection) {
     t, _ := s.Attr("type")
     if t == "application/ld+json" {
       // could this be parsed in a better way ?
       // it's json-ld
+      fmt.Println(s.Text())
       if strings.Contains(s.Text(), "\"isLiveBroadcast\":true") {
         islive = true
       }
     }
   })
 
-  return islive
+  return islive, nil
 }
