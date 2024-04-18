bf06d59454d6a1f4a57586f42de3a4095a02320f
Jon Doe <kaz00@vivaldi.net>
2024-04-18 13:58:49 +0300

    Added a spinner and what is being updated in the status line. Now the streamerlist gets loaded at start so you have to restart the TUI to recognize new changes.+Other minor stuff.

--- tui.go
+++ tui.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os/exec"
 	"strconv"
@@ -11,6 +10,7 @@ import (
 
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -68,11 +68,19 @@ type keyMap struct {
 	Quit      key.Binding
 }
 
+type updatedMsg string
+
+var Rows []table.Row
+
 type model struct {
-	table   table.Model
-	keys    keyMap
-	help    help.Model
-	updated string
+	table        table.Model
+	keys         keyMap
+	help         help.Model
+	updated      string
+	spinner      spinner.Model
+	spin         bool
+	index        int
+	streamerlist []string
 }
 
 func (k keyMap) ShortHelp() []key.Binding {
@@ -86,14 +94,11 @@ func (k keyMap) FullHelp() [][]key.Binding {
 	}
 }
 
-// Maybe I should initialize the table here ?
-// But the example doesn't do that...
-// Made it to clearscreen on init so I don't have to do it twice
-// when updateing the tables throught updateStreamers()
 func (m model) Init() tea.Cmd { return tea.ClearScreen }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -103,23 +108,39 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, tea.Println(err)
 			}
 		case "f5": // refresh the table
-			t := "Last update: " + time.Now().Format("15:04:05")
-			m.table.SetRows(updateStreamers())
-			// Can't get rid of tea.Printf() because it breaks the table on refresh, why?!
-			// seems like forcing a ClearScreen works as a workaround.
-			// updating the model doesn't help the table breaks still
-			return model{table: m.table, keys: m.keys, help: m.help, updated: t}, tea.ClearScreen
+			m.spin = true
+			m.updated = "Checking " + m.streamerlist[0] + "... "
+			return m, tea.Batch(refreshStreamer(m.streamerlist[0], 0), m.spinner.Tick)
 		case "q", "ctrl+c", "ctrl+d": // quit
 			return m, tea.Quit
-		case "enter": // select and open to default browser TODO: make it OS agnostic ?
-			selection := "You selected " + m.table.SelectedRow()[1]
-			err := exec.Command("xdg-open", m.table.SelectedRow()[3]).Start()
+		case "enter": // select and open to default browser
+			m.updated = "You selected " + m.table.SelectedRow()[1]
+			err := exec.Command("xdg-open", url + m.table.SelectedRow()[1]).Start()
 			if err != nil {
 				return m, tea.Println(err)
 			}
-			return model{table: m.table, keys: m.keys, help: m.help, updated: selection}, nil
+			return m, nil
 		}
+	case updatedMsg:
+		if m.index >= len(m.streamerlist)-1 {
+			m.spin = false
+			m.table.SetRows(Rows)
+			m.index = 0
+			m.updated = "Last updated: " + time.Now().Format("15:04:05")
+			Rows = nil
+			return m, cmd
+		}
+		m.index++
+		m.updated = "Checking " + m.streamerlist[m.index] + "... "
+		return m, refreshStreamer(m.streamerlist[m.index], m.index)
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		if m.spin {
+			m.spinner, cmd = m.spinner.Update(msg)
+		}
+		return m, cmd
 	}
+
 	m.table, cmd = m.table.Update(msg)
 	return m, cmd
 }
@@ -128,6 +149,9 @@ func (m model) View() string {
 	tbl := baseStyle.Render(m.table.View())
 	hlp := helpStyle.Render(m.help.View(m.keys))
 	status := statusStyle.Render(m.updated)
+	if m.spin {
+		status = statusStyle.Render(m.spinner.View() + m.updated)
+	}
 	// +4 for the outer top/bottom and header
 	height := m.table.Height() + 4 - strings.Count(tbl, "\n") - strings.Count(hlp, "\n")
 
@@ -136,7 +160,7 @@ func (m model) View() string {
 
 // This is the "main"
 func startTUI() error {
-	// create the collumns and header titles
+	// create the columns and header titles
 	columns := []table.Column{
 		{Title: "#", Width: 3},
 		{Title: "Streamer", Width: 22},
@@ -145,13 +169,20 @@ func startTUI() error {
 	}
 
 	// create the rows
+    // TODO: Change the 1st time of checking streamers to construct the table 
+    // so it doesn't use the old code
 	rows := updateStreamers()
-	var tHeight int // max height of the table
-	if len(rows) >= 15 {
-		tHeight = 15
-	} else {
-		tHeight = len(rows)
-	}
+    // It helps to add the streamerlist at the start to be able to show progress
+    // updates but also if you add a streamer while the TUI is open you have to
+    // restart.
+	streamerlist := initStreamerList()
+	// var tHeight int // max height of the table
+	// if len(rows) >= 15 {
+	// 	tHeight = 15
+	// } else {
+	// 	tHeight = len(rows)
+	// }
+    tHeight := 15 //Temp change so it will always until I decide what to do with it
 
 	// make a table style
 	s := table.DefaultStyles()
@@ -180,11 +211,19 @@ func startTUI() error {
 
 	u := "Last update: " + time.Now().Format("15:04:05")
 
+	spin := spinner.New()
+	spin.Spinner = spinner.Dot
+	spin.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
 	// run the model
 	m := model{
-		table:   t,
-		help:    h,
-		updated: u,
+		table:        t,
+		help:         h,
+		updated:      u,
+		spinner:      spin,
+		spin:         false,
+		index:        0,
+		streamerlist: streamerlist,
 	}
 	m.keys = keys
 	if _, err := tea.NewProgram(m).Run(); err != nil {
@@ -194,6 +233,42 @@ func startTUI() error {
 	return nil
 }
 
+// Takes the streamer string and the index, update the Rows and 
+// returns a tea.Cmd to show progress
+func refreshStreamer(streamer string, index int) tea.Cmd {
+	d := 300 * time.Millisecond
+	resp, _ := getResponse(url + streamer)
+
+	if resp != nil {
+		isLive, title, _ := parse(resp)
+		defer resp.Body.Close()
+
+		if isLive {
+			Rows = append(Rows, table.Row{strconv.Itoa(index), streamer, "LIVE", title})
+		} else {
+			Rows = append(Rows, table.Row{strconv.Itoa(index), streamer, "OFFLINE", title})
+		}
+	}
+	return tea.Tick(d, func(t time.Time) tea.Msg {
+		return updatedMsg("done")
+	})
+}
+
+// Constructs a list of all the streamers to go with the model
+func initStreamerList() []string {
+	var streamerlist []string
+	f := openStreamerlist()
+
+	fScanner := bufio.NewScanner(f)
+	fScanner.Split(bufio.ScanLines)
+	for fScanner.Scan() {
+		streamerlist = append(streamerlist, fScanner.Text())
+	}
+
+	return streamerlist
+}
+
+// Soon hopefully this will not be needed...
 func updateStreamers() (rows []table.Row) {
 	f := openStreamerlist()
 
@@ -205,7 +280,7 @@ func updateStreamers() (rows []table.Row) {
 		// probably I should use bubbletea to print those
 		// but it works fine like that so I will leave it
 		// until it bites my ass
-		fmt.Println("Checking ", yellow+streamer+reset, "...")
+		// fmt.Println("Checking ", yellow+streamer+reset, "...")
 
 		resp, err := getResponse(url + streamer)
 		if err != nil {
@@ -230,7 +305,6 @@ func updateStreamers() (rows []table.Row) {
 		time.Sleep(300 * time.Millisecond)
 	}
 	defer f.Close()
-	// clearTerm()
 
 	return rows
 }
